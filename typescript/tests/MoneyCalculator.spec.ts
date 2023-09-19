c2652e68073c176a8641e4218c2075f1326ae053
Green-2 <olivier.pergande@gmail.com>
2023-09-19 09:30:43 +0200

    other variables moneycalculator

--- typescript/tests/MoneyCalculator.spec.ts
+++ typescript/tests/MoneyCalculator.spec.ts
@@ -2,15 +2,18 @@ import * as MoneyCalculator from '../src/MoneyCalculator'
 
 describe('Money', function () {
   test('add in usd returns number', () => {
-    expect(MoneyCalculator.add(5, 10)).toBeNumber()
-    expect(MoneyCalculator.add(5, 10)).not.toBeNull()
+    const fivePlusTenAmount = MoneyCalculator.add(5, 10)
+    expect(fivePlusTenAmount).toBeNumber()
+    expect(fivePlusTenAmount).not.toBeNull()
   })
 
   test('multiply in eur returns positive number', () => {
-    expect(MoneyCalculator.times(10, 2)).toBeGreaterThan(0)
+    const tenDividedByTwoNumber = MoneyCalculator.times(10, 2)
+    expect(tenDividedByTwoNumber).toBeGreaterThan(0)
   })
 
   test('divide in korean won returns number', () => {
-    expect(1000.5).toBe(MoneyCalculator.divide(4002, 4))
+    const fourThousandDividedByFourNumber = MoneyCalculator.divide(4002, 4)
+    expect(1000.5).toBe(fourThousandDividedByFourNumber)
   })
 })
