77154d5ce6b94e2d3b2c7ce5e84efea86c018cbd
Green-2 <olivier.pergande@gmail.com>
2023-09-19 09:12:28 +0200

    other variables

--- typescript/tests/Bank.spec.ts
+++ typescript/tests/Bank.spec.ts
@@ -3,25 +3,26 @@ import { Bank } from '../src/Bank'
 import { MissingExchangeRateError } from '../src/MissingExchangeRateError'
 
 describe('Bank', function () {
-  const bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2);
+  const bank1point2 = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2)
   test('convert from eur to usd returns number', () => {
-    expect(bank.convert(Currency.EUR, Currency.USD, 10)).toBe(12)
+    expect(bank1point2.convert(Currency.EUR, Currency.USD, 10)).toBe(12)
   })
 
   test('convert from usd to usd returns same value', () => {
-    expect(bank.convert(Currency.USD, Currency.USD, 10)).toBe(10)
+    expect(bank1point2.convert(Currency.USD, Currency.USD, 10)).toBe(10)
   })
 
   test('convert throws error in case of missing exchange rates', () => {
-    expect(() => bank.convert(Currency.EUR, Currency.KRW, 10))
+    expect(() => bank1point2.convert(Currency.EUR, Currency.KRW, 10))
       .toThrow(MissingExchangeRateError)
   })
 
   test('convert with different exchange rates returns different numbers', () => {
-    expect(bank.convert(Currency.EUR, Currency.USD, 10)).toBe(12)
+    expect(bank1point2.convert(Currency.EUR, Currency.USD, 10)).toBe(12)
 
-    expect(Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.3).convert(Currency.EUR, Currency.USD, 10)).toBe(13)
-
-    expect(Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.5).convert(Currency.EUR, Currency.USD, 10)).toBe(15)
+    const bank1point3 = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.3)
+    const bank1point5 = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.5)
+    expect(bank1point3.convert(Currency.EUR, Currency.USD, 10)).toBe(13)
+    expect(bank1point5.convert(Currency.EUR, Currency.USD, 10)).toBe(15)
   })
 })
