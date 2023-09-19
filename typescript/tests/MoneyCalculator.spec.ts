4e668d669d0cf4fb951c320f0e482a98ed22cdd5
ccrispel <clement.crispel@gmail.com>
2023-09-19 10:35:36 +0200

    modif test

--- typescript/tests/MoneyCalculator.spec.ts
+++ typescript/tests/MoneyCalculator.spec.ts
@@ -2,26 +2,12 @@ import * as MoneyCalculator from '../src/MoneyCalculator'
 
 describe('Money', function () {
   test('add in usd returns number', () => {
-<<<<<<< HEAD
-    expect(MoneyCalculator.add(5, 10)).toBeNumber()
-    expect(MoneyCalculator.add(5, 10)).not.toBeNull()
-    expect(MoneyCalculator.add(5, 10)).toBeGreaterThan(5)
+    expect(MoneyCalculator.add(5, 10)).toBe(15)
   })
 
   test('multiply in eur returns positive number', () => {
-    expect(MoneyCalculator.times(10, 2)).toBeGreaterThan(0)
-    expect(MoneyCalculator.times(10, 2)).toBeGreaterThan(10)
+    expect(MoneyCalculator.times(10, 2)).toBe(20)
 
-=======
-    const fivePlusTenAmount = MoneyCalculator.add(5, 10)
-    expect(fivePlusTenAmount).toBeNumber()
-    expect(fivePlusTenAmount).not.toBeNull()
-  })
-
-  test('multiply in eur returns positive number', () => {
-    const tenDividedByTwoNumber = MoneyCalculator.times(10, 2)
-    expect(tenDividedByTwoNumber).toBeGreaterThan(0)
->>>>>>> c2652e68073c176a8641e4218c2075f1326ae053
   })
 
   test('divide in korean won returns number', () => {
