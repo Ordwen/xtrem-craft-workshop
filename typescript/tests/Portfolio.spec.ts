c5b1140127262e91ebbf77052a7d49e370398e74
ccrispel <clement.crispel@gmail.com>
2023-10-17 08:44:53 +0200

    portfolio test

--- typescript/tests/Portfolio.spec.ts
+++ typescript/tests/Portfolio.spec.ts
@@ -3,6 +3,23 @@ import { Bank } from '../src/Bank'
 import Portfolio from '../src/Portfolio'
 
 describe('Portfolio', function () {
+
+  test('Empty portfolio', () => {
+    const portfolio = new Portfolio()
+    const bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2)
+    const evaluation = portfolio.evaluate(Currency.USD, bank)
+    expect(evaluation).toBe(0)
+  })
+
+  test('Add to portfolio', () => {
+    const portfolio = new Portfolio()
+    portfolio.add(10, Currency.USD)
+    const bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2)
+    const evaluation = portfolio.evaluate(Currency.USD, bank)
+    expect(evaluation).toBe(10)
+  })
+
+  
   test('10 USD + 5 USD = 15 US', () => {
     const portfolio = new Portfolio()
     portfolio.add(10, Currency.USD)
@@ -10,6 +27,7 @@ describe('Portfolio', function () {
     const evaluation = portfolio.evaluate(Currency.USD, Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2))
     expect(evaluation).toBe(15)
   })
+
   test('5 USD + 10 EUR = 17 USD', () => {
     const portfolio = new Portfolio()
     const bank = Bank.createBankWithExchangeRate(Currency.EUR, Currency.USD, 1.2)
