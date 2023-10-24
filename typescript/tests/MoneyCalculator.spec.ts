194240aafc4114d6108eec16bd43061032dcec72
Benjamin Chew <benjaminchew4@gmail.com>
2023-10-24 09:17:21 +0200

    feat: added test

--- typescript/tests/MoneyCalculator.spec.ts
+++ typescript/tests/MoneyCalculator.spec.ts
@@ -1,4 +1,6 @@
 import * as MoneyCalculator from '../src/MoneyCalculator'
+import { Currency } from '../src/Currency'
+import { Money } from '../src/Money'
 
 describe('Money', function () {
   test('add in usd returns number', () => {
@@ -7,11 +9,18 @@ describe('Money', function () {
 
   test('multiply in eur returns positive number', () => {
     expect(MoneyCalculator.times(10, 2)).toBe(20)
-
   })
 
   test('divide in korean won returns number', () => {
     const fourThousandDividedByFourNumber = MoneyCalculator.divide(4002, 4)
     expect(1000.5).toBe(fourThousandDividedByFourNumber)
+  })
+
+  test('should add monies when currencies are the same', () => {
+    const money = new Money(5, Currency.USD)
+    const sum = money.add(new Money(10, Currency.USD))
+
+    expect(sum).toEqual(new Money(15, Currency.USD))
+    expect(money).toEqual(new Money(5, Currency.USD))
   })
 })
