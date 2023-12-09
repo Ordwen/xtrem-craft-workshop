79e690b2155cc84232071b2faaddf1e03a4645f2
Thibaud Bergez-Vigneau <thibaud.bergez-vigneau@u-bordeaux.fr>
2023-12-09 15:04:52 +0100

    change exchange rate usage in tests

--- typescript/tests/Bank.spec.ts
+++ typescript/tests/Bank.spec.ts
@@ -1,60 +1,71 @@
 import { Currency } from '../src/Currency'
 import { Bank } from '../src/Bank'
+import { BankBuilder } from '../src/BankBuilder'
 import { Money } from '../src/Money'
 import { MissingExchangeRateError } from '../src/MissingExchangeRateError'
 
 describe('Bank', function () {
-  const bank1point2 = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
+  const bank1point2 = BankBuilder.aBank()
+    .withPivotCurrency(Currency.EUR)
+    .withExchangeRate(Currency.USD, 1.2)
+    .build()
   test('convert from eur to usd returns number', () => {
-    var money = new Money(10,Currency.EUR)
+    var money = new Money(10, Currency.EUR)
     const valueAfterConversion = bank1point2.convert(Currency.USD, money)
     expect(valueAfterConversion).toBe(12)
   })
 
   test('convert from usd to usd returns same value', () => {
-    var money = new Money(10,Currency.USD)
-    const valueAfterConversion = bank1point2.convert(Currency.USD,money)
+    var money = new Money(10, Currency.USD)
+    const valueAfterConversion = bank1point2.convert(Currency.USD, money)
     expect(valueAfterConversion).toBe(10)
   })
 
   test('convert throws error in case of missing exchange rates', () => {
-    var money = new Money(10,Currency.EUR)
-    expect(() => bank1point2.convert(Currency.KRW,money)).toThrowWithMessage(MissingExchangeRateError, 'No exchange rate from EUR to KRW')
+    var money = new Money(10, Currency.EUR)
+    expect(() => bank1point2.convert(Currency.KRW, money)).toThrowWithMessage(MissingExchangeRateError, 'No exchange rate from EUR to KRW')
   })
 
   test('convert with different exchange rates returns different numbers', () => {
-    const bank1point3 = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.3)
-    const bank1point5 = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.5)
+    const bank1point3 = BankBuilder.aBank()
+      .withPivotCurrency(Currency.EUR)
+      .withExchangeRate(Currency.USD, 1.3)
+      .build()
+    const bank1point5 = BankBuilder.aBank()
+      .withPivotCurrency(Currency.EUR)
+      .withExchangeRate(Currency.USD, 1.5)
+      .build()
     let valueAfterConversion: number
-    var money = new Money(10,Currency.EUR)
-    valueAfterConversion = bank1point2.convert(Currency.USD,money)
+    var money = new Money(10, Currency.EUR)
+    valueAfterConversion = bank1point2.convert(Currency.USD, money)
     expect(valueAfterConversion).toBe(12)
-    money = new Money(10,Currency.EUR)
-    valueAfterConversion = bank1point3.convert(Currency.USD,money)
+    money = new Money(10, Currency.EUR)
+    valueAfterConversion = bank1point3.convert(Currency.USD, money)
     expect(valueAfterConversion).toBe(13)
-    money = new Money(10,Currency.EUR)
-    valueAfterConversion = bank1point5.convert(Currency.USD,money)
+    money = new Money(10, Currency.EUR)
+    valueAfterConversion = bank1point5.convert(Currency.USD, money)
     expect(valueAfterConversion).toBe(15)
   })
 
   test('convert with missing exchange rate uses public exchange rate', () => {
-    const bank = Bank.withExchangeRate(Currency.EUR, Currency.USD, 1.2)
-    var money = new Money(10,Currency.EUR)
-    const valueAfterConvertion = bank.convert(Currency.USD,money)
+    const bank = BankBuilder.aBank()
+      .withPivotCurrency(Currency.EUR)
+      .withExchangeRate(Currency.USD, 1.2)
+      .build()
+    var money = new Money(10, Currency.EUR)
+    const valueAfterConvertion = bank.convert(Currency.USD, money)
     expect(valueAfterConvertion).toBe(12)
 
-    money = new Money(10,Currency.EUR)
-    expect(() => bank.convert(Currency.KRW,money)).toThrow(MissingExchangeRateError)
-    
-    money = new Money(10,Currency.KRW)
-    expect(() => bank.convert(Currency.EUR,money)).toThrow(MissingExchangeRateError)
-    
-    expect(() => bank.convert(Currency.USD,money)).toThrow(MissingExchangeRateError)
-    
-
-    money = new Money(10,Currency.USD)
+    money = new Money(10, Currency.EUR)
     expect(() => bank.convert(Currency.KRW, money)).toThrow(MissingExchangeRateError)
-    
+
+    money = new Money(10, Currency.KRW)
     expect(() => bank.convert(Currency.EUR, money)).toThrow(MissingExchangeRateError)
+
+    expect(() => bank.convert(Currency.USD, money)).toThrow(MissingExchangeRateError)
+
+
+    money = new Money(10, Currency.USD)
+    expect(() => bank.convert(Currency.KRW, money)).toThrow(MissingExchangeRateError)
   })
 })
